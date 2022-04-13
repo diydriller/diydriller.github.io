e1b31fba50d35cada8bf992e34db39952cda9926
diydriller <diydriller@users.noreply.github.com>
2022-04-13 07:14:56 +0000

    deploy: 4be4525ff79e2f1b37654b43623a733abe23a0fc

---
+++ _nuxt/static/1649834095/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649834095",layout:"default",error:a,state:{},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bfb4b94a"}}}}(null,"\u002F"));
