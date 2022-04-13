4f842331cdcbc458514e61d47778fe1b6f441236
diydriller <diydriller@users.noreply.github.com>
2022-04-13 07:40:51 +0000

    deploy: bfa9327475e5f85f1133cf440766f04517728b8d

--- _nuxt/static/1649834894/backend/nodejs/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdiydriller.github.io\u002F_nuxt\u002Fstatic\u002F1649834894",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fbackend\u002Fnodejs",config:{_app:{basePath:"\u002Fdiydriller.github.io\u002F",assetsPath:"\u002Fdiydriller.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"427b57dd"}}}}(null));
